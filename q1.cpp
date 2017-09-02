4d7b00457381e4f17a3df05590d9dc4970044a1f
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-02 19:17:29 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b;
+	cout << "enter the two numbers: \n";
+	cin >> a >> b;
+	cout << "the greater number is " << max (a,b);
+	
+	
+	return 0;
+}
