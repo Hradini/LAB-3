024994250d8467f446417208140653320e76260c
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-02 19:49:32 +0530

    Create q6.cpp

---
+++ q6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "enter a number: \n";
+	cin >> a;
+	b = a % 55;
+	if ( b = 0) {
+		cout << "the number is divisible by 5 and 11";
+		} else {
+			cout << "the number is not divisible by 5 and 11";	
+		}
+		
+		
+	
+	
+	return 0;
+}
