e478c8e063de5909fe969927ff686a3b1b1a6b19
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-02 19:40:39 +0530

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a;
+	cout << "enter a number: \n";
+	cin >> a;
+	if ( a > 0) {
+		cout << "the number is positive";
+		} else if (a = 0) {
+			cout << "the number is zero" ;
+		} else {
+			cout << "the number is negative";
+		}
+		
+		
+	
+	
+	return 0;
+}
