aeee5f7c9e380aac1e2a7feacb6af111f1922f9c
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-02 20:18:52 +0530

    Create q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "enter a number: \n";
+	cin >> a;
+	b = a % 2;
+	if ( b == 0) {
+		cout << "the number is even";
+		} else {
+			cout << "the number is odd";	
+		}
+		
+		
+	
+	
+	return 0;
+}
