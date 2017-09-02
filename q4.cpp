513e061c5420bf2f3d0e37e925cccd4ee5595db6
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-02 20:12:41 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -6,7 +6,7 @@ int main() {
 	cout << "enter a number: \n";
 	cin >> a;
 	b = a % 55;
-	if ( b = 0) {
+	if ( b == 0) {
 		cout << "the number is divisible by 5 and 11";
 		} else {
 			cout << "the number is not divisible by 5 and 11";	
