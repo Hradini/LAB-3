3433b1ec91bb6c43c89d04fbef83a8d17c4a1f49
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-06 09:35:59 +0530

    Update q11.cpp

--- q11.cpp
+++ q11.cpp
@@ -3,26 +3,28 @@ using namespace std;
 
 int main() {
 	int a;
+	cout << "please enter a number: "<< endl;
+	cin >> a;
 	switch(a) {
 	case '0' :
          cout << "sunday" << endl; 
          break;
-         case '1' :
+         case 1 :
          cout << "monday" << endl; 
          break;
-         case '2' :
+         case 2 :
          cout << "tuesday" << endl; 
          break;
-         case '3' :
+         case 3 :
          cout << "wednesday" << endl; 
          break;
-         case '4' :
+         case 4 :
          cout << "thursday" << endl; 
          break;
-         case '5' :
+         case 5 :
          cout << "friday" << endl; 
          break;
-         case '6' :
+         case 6 :
          cout << "saturday" << endl; 
          break;
          
