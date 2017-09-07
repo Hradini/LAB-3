b5e9502897c61bd61e8a2baa0663349a2b689c35
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-07 17:42:44 +0530

    Create q23.cpp

---
+++ q23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i;
+	
+	
+	cout << "enter the upper limit \n";
+	cin >> n;
+	i = n;
+	while ( i >= 1){
+	cout << i << "\n";
+	i--;
+	}
+	
+	
+	
+	return 0;
+}
