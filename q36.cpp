8c57082f00c5d5f8c8d03113c4dcceb540a6c017
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-08 17:50:46 +0530

    Create q36.cpp

---
+++ q36.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num,pro, n1;
+	sum = 1;
+	cout << "enter the positive integer: \n";
+	cin >> num;
+	n1 = num;
+	while (n1 != 0){
+		n1 = n1 % 10;
+		pro = pro * n1;
+		num = num /10;
+		n1 = num;
+		
+	}
+	
+	cout << pro;
+	
+	return 0;
+}
