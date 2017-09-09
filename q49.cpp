ce538ecad849779c3cc16538ba9814ec539a8f6f
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-09 16:39:31 +0530

    Create q49.cpp

---
+++ q49.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, i, n2, j,sum=0;
+  bool isPrime = true;
+
+  cout << "Enter a positive integer: \n";
+  cin >> n2;
+  for ( j = 1; j<= n2; j++){
+	isPrime = true;
+	for(i = 2; i <= (j/2); i++){
+		if(j % i == 0){
+		isPrime = false;
+		break;
+		}
+	}
+	if (isPrime){
+	cout << j << "  ";
+	sum = sum + j;
+	}
+  }
+	cout << sum;
+  return 0;
+}
