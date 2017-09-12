a38876ef5e76ae25e3fe861bb92eeb646946ece5
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-12 19:34:45 +0530

    Create 56.cpp

---
+++ 56.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num, n,n1,n2, i, f=1, sof=0;
+	cin >> n2;
+	for(num =1;num <=n2;num++){
+		n=num;
+		while(num !=0){
+			f =1;
+			n1 = num%10;
+			num = num/10;
+			for (i=1; i<=n1;i++){
+				f=f*i;
+				}
+				
+			sof = sof + f;
+			
+		}
+		cout << sof << endl;
+		if ( sof == n){
+			cout << "it is a strong number";
+		}
+	}		
+	return 0;
+}
