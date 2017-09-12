d4e51cc87bf68a4ad04306ee0761db99a5c3ab9d
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-12 19:48:37 +0530

    Update 56.cpp

--- 56.cpp
+++ 56.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main() {
-	int num, n,n1,n2, i, f=1, sof=0;
+	int num, n,n1,n2,j, i, f=1, sof=0;
 	cin >> n2;
-	for(num =1;num <=n2;num++){
-		n=num;
+	for(j =1;j <=n2;j++){
+		n=j;
+		num=j;
+		sof=0;
 		while(num !=0){
 			f =1;
 			n1 = num%10;
@@ -13,14 +15,13 @@ int main() {
 			for (i=1; i<=n1;i++){
 				f=f*i;
 				}
-				
 			sof = sof + f;
-			
-		}
-		cout << sof << endl;
+			}
+	
 		if ( sof == n){
-			cout << "it is a strong number";
+			cout << n << " is a strong number\n";
 		}
+		
 	}		
 	return 0;
 }
