f75cf760ab0a7ac4a120e9699addf353c6beeb51
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-14 17:59:27 +0530

    Update q62.cpp

--- q62.cpp
+++ q62.cpp
@@ -3,18 +3,14 @@
 using namespace std;
 
 int main() {
-	int binary; decimal; hex;
+	int binary, decimalNumber,i =0,j=0,d=0, count=0;
 	cin >> binary;
 	int store = binary;
 	 while (store !=0){
     	store = store/10;
     	++count;
     	
-	i = count 
-	
-	
-	
-	for ( i=0;  ; i++){
+	for ( i=0; i <=(count/4)+1 ; i++){
 		d = store % 10000;
 			
 				while(d != 0)
@@ -23,8 +19,32 @@ int main() {
 			        ++j;
 			        d/=10;
 			    } j=1;
-				decimal += decimalNumber* pow(10000,i);
+					switch(decimalNumber){
+						case 10 :
+						decimalNumber = 'A';
+						break;
+						case 11 :
+						decimalNumber = 'B';
+						break;
+						case 12 :
+						decimalNumber = 'C';
+						break;
+						case 13 :
+						decimalNumber = 'D';
+						break;
+						case 14 :
+						decimalNumber = 'E';
+						break;
+						case 15 :
+						decimalNumber = 'F';
+						break;
+					} cout << decimalNumber;
+				
+				decimalNumber=0;
+				
+				
 		  store = store / 10000;
 	}
 	return 0;
+	}
 }
