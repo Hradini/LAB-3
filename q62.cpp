e151fcee8b2f13bf8b6c9a3ce1652da0bbca06b0
Hradini <31266306+Hradini@users.noreply.github.com>
2017-09-14 17:22:17 +0530

    Create q62.cpp

---
+++ q62.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int binary; decimal; hex;
+	cin >> binary;
+	int store = binary;
+	 while (store !=0){
+    	store = store/10;
+    	++count;
+    	
+	i = count 
+	
+	
+	
+	for ( i=0;  ; i++){
+		d = store % 10000;
+			
+				while(d != 0)
+			    {
+			        decimalNumber += (d%10) * pow(2,i);
+			        ++j;
+			        d/=10;
+			    } j=1;
+				decimal += decimalNumber* pow(10000,i);
+		  store = store / 10000;
+	}
+	return 0;
+}
